f1e640f31a5d06915319b0c72a58eae35a3a5b0c
Jonathan Easterman <jonathaneasterman@gmail.com>
2017-08-02 16:03:24 -0700

    je - Remove Activerecord relation between applications and access tokens

--- lib/doorkeeper/models/application_mixin.rb
+++ lib/doorkeeper/models/application_mixin.rb
@@ -8,7 +8,6 @@ module Doorkeeper
 
     included do
       has_many :access_grants, dependent: :delete_all, class_name: 'Doorkeeper::AccessGrant'
-      has_many :access_tokens, dependent: :delete_all, class_name: 'Doorkeeper::AccessToken'
 
       validates :name, :secret, :uid, presence: true
       validates :uid, uniqueness: true
